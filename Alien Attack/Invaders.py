7887d4b185598a161c2dc20879afb7621b0bb3c2
veerjakhar <155930805+veerjakhar@users.noreply.github.com>
2025-05-30 18:00:58 -0700

    Add files via upload

---
+++ Alien Attack/Invaders.py
@@ -0,0 +1,31 @@
+import pgzrun
+from random import randint
+
+HEIGHT = 400
+WIDTH = 400
+message = "Attack on alien"
+TITLE = "Alien Attack"
+alien = Actor('alien.png')
+
+
+def draw():
+    screen.clear()
+    screen.fill( color = (128, 0, 0))
+    alien.draw()
+    screen.draw.text(message, center = (200, 10), fontsize = 30)
+
+    def place_alien():
+        alien.x = randint(50, WIDTH - 50)
+        alien.y = randint(50, HEIGHT - 50)
+
+    def on_mouse_down(pos):
+        global message
+        if alien.collidepoint(pos):
+            message = "Good Shot!"
+            place_alien()
+        else:
+            message = "You FALIED!!!!!!!"
+
+
+
+pgzrun.go()
