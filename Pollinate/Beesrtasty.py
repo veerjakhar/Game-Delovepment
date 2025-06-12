4750fddc188fd08cc759959fdcc17c4405be0226
veerjakhar <155930805+veerjakhar@users.noreply.github.com>
2025-06-11 19:00:16 -0700

    Add files via upload

--- Pollinate/Beesrtasty.py
+++ Pollinate/Beesrtasty.py
@@ -4,8 +4,9 @@ from random import randint
 HEIGHT = 500
 WIDTH = 600
 score = 0
+game_over = False
 
-masseage = "Pollinate... or else"
+message = "Pollinate... or else"
 bee = Actor('bee.png')
 bee.pos = 100,100
 flower = Actor('flower.png')
@@ -17,10 +18,15 @@ def draw():
     bee.draw()
     flower.draw()
     screen.draw.text("Score: " + str(score), color = "black", topleft = (10,10))
-
+    if game_over == True:
+        screen.draw.text("Time is up your final score is: " + str(score), color = "black", midtop = (WIDTH/2, 10), fontsize = 40)
 def place_flower():
     flower.x =  randint(70, (WIDTH  - 70))
     flower.y = randint(70, (HEIGHT - 70))
+
+def time_up():
+    global game_over
+    game_over = True
 
 def update():
     global score
@@ -39,6 +45,5 @@ def update():
         place_flower()
    
 
-
-
+clock.schedule(time_up, 10.0)
 pgzrun.go()
---
+++ Pollinate/Ichor/Dandy.py
@@ -0,0 +1,10 @@
+import pgzrun
+from random import randint
+
+HEIGHT = 400
+WIDTH = 405
+
+dandy = Actor(Dandy.png)
+dandy.pos = 100, 100
+ichor = Actor(ichor.png)
+ichor.pos = 200, 200
