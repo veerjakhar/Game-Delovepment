1bfafcffb6d4a5ea0dfec379ef65b31eb7a81ead
veerjakhar <155930805+veerjakhar@users.noreply.github.com>
2025-06-11 18:22:20 -0700

    Add files via upload

--- Pollinate/Beesrtasty.py
+++ Pollinate/Beesrtasty.py
@@ -3,6 +3,7 @@ from random import randint
 
 HEIGHT = 500
 WIDTH = 600
+score = 0
 
 masseage = "Pollinate... or else"
 bee = Actor('bee.png')
@@ -15,8 +16,14 @@ def draw():
     screen.blit("background", (0,0))
     bee.draw()
     flower.draw()
+    screen.draw.text("Score: " + str(score), color = "black", topleft = (10,10))
+
+def place_flower():
+    flower.x =  randint(70, (WIDTH  - 70))
+    flower.y = randint(70, (HEIGHT - 70))
 
 def update():
+    global score
     if keyboard.left:
         bee.x = bee.x - 2
     elif keyboard.right:
@@ -25,6 +32,13 @@ def update():
         bee.y = bee.y - 2
     elif keyboard.down:
         bee.y = bee.y + 2
+    
+    flower_collected = bee.colliderect(flower)
+    if flower_collected:
+        score += 10
+        place_flower()
+   
+
 
 
 pgzrun.go()
