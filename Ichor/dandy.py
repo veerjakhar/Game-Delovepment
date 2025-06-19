2e7d8aefddb76d035fa1ed3a9ca510107624ca11
veerjakhar <155930805+veerjakhar@users.noreply.github.com>
2025-06-18 18:58:58 -0700

    Add files via upload

--- Ichor/dandy.py
+++ Ichor/dandy.py
@@ -8,6 +8,7 @@ twisted = False
 
 dandy = Actor("dandy.png")
 dandy.pos = 100, 100
+dandy.height = 10
 ichor = Actor("ichor.png")
 ichor.pos = 200, 200
 
@@ -15,9 +16,9 @@ def draw():
     screen.blit("fakedw", (0,0))
     dandy.draw()
     ichor.draw()
-    screen.draw.text("Score: " + str(score), color = ("black"), topleft = (10, 10))
+    screen.draw.text("Score: " + str(score), color = ("white"), topleft = (10, 10))
     if twisted == True:
-        screen.draw.text("Your final score is: " + str(score), color = ("black"), midtop = (WIDTH/2, 10), fontsize = 40)
+        screen.draw.text("Your final score is: " + str(score), color = ("white"), midtop = (WIDTH/2, 10), fontsize = 40)
 
 def gtg():
     global twisted
@@ -28,20 +29,22 @@ def machine():
     ichor.x = randint(70, (WIDTH - 70))
     ichor.y = randint(70, (HEIGHT - 70))
 
-def move():
+def update():
+    global score
     if keyboard.left:
-        ichor.x = ichor.x - 2
+        dandy.x = dandy.x - 2
     elif keyboard.right:
-        ichor.x = ichor.x + 2
-    elif keyboard.up:
-        ichor.y = ichor.y + 2
+        dandy.x = dandy.x + 2
     elif keyboard.down:
-        ichor.y = ichor.y - 2
-
-    ichor_col = ichor.colliderect(flower)
-    if ichor_col:
+        dandy.y = dandy.y + 2
+    elif keyboard.up:
+        dandy.y = dandy.y - 2
+    ichor_collect = dandy.colliderect(ichor)
+    
+    if ichor_collect:
         score += 10
         machine()
 
-clock.schedule(gtg, 10)
+
+clock.schedule(gtg, 60)
 pgzrun.go()
